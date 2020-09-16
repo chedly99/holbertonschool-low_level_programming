d4fa10d59de8c52bde9187ab0871dc8e0f5fc9b8
chedly99 <2276@holbertonschool.com>
2020-09-16 20:58:16 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
+/**
+*main - void
+*return: 0
+*/
 void main(void)
-{puts("\"Programming is like building a multilingual puzzle"); }
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
