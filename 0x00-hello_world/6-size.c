ba42b0072dd50b02708e679b5a2efc2d1537d22a
chedly99 <2276@holbertonschool.com>
2020-09-16 22:54:11 +0100

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - main
+* Return: 0
+*/
+int main(void)
+{printf("Size of char: %ld bytes\n", sizeof(char));
+printf("Size of int: %ld bytes\n", sizeof(int));
+printf("Size of long int: %ld bytes\n", sizeof(long int));
+printf("Size of long long int: %ld byte\n", sizeof(long long int));
+printf("Size of float: %ld bytes\n", sizeof(float));
+return (0);
+}
