1da41790c748d8a562b40b5877d27248d16ea6f4
chedly99 <2276@holbertonschool.com>
2020-09-17 21:26:23 +0100

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - main
+* Return: 0
+*/
+int main(void)
+{
+int ch;
+char chh;
+for (ch = '0' ; ch <= '9' ; ch++)
+putchar(ch);
+for (chh = 'a' ; chh <= 'f' ; chh++)
+putchar(chh);
+putchar ('\n');
+return (0);
+}
