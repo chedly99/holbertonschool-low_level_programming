ec981301cee4e01ca11da830eadae84cfce5eed0
chedly99 <2276@holbertonschool.com>
2020-09-17 22:00:57 +0100

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - main
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+if (ch != '9' ) {
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar ('\n');
+return (0);
+}
