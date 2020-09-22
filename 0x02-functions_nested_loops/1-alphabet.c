08295f7e383d7b0155e1d753b731507de632ff7c
chedly99 <2276@holbertonschool.com>
2020-09-22 11:49:24 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
-* main - print_alphabet
+* print_alphabet - void
 */
 void print_alphabet(void)
 {
-char ch = 'a';
+int ch = 'a';
 char n = '\n';
 for (ch = 'a' ; ch <= 'z' ; ch++)
 _putchar(ch);
