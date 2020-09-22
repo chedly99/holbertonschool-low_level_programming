4634f6b62b56c766c8d4b89a271053dc3faede0f
chedly99 <2276@holbertonschool.com>
2020-09-22 22:18:25 +0100

    print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*print_to_98 - print to 89
+*@n : int
+*Return:
+*/
+void print_to_98(int n)
+{
+if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n = n - 1;
+}
+}
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
+}
+printf("\n");
+}
