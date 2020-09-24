6f4ee938933b0acd528e6445b8d70d29fce152c3
chedly99 <2276@holbertonschool.com>
2020-09-24 22:49:48 +0100

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - void
+*FizzBuzz printing
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for(i = 1 ; i <=100; i++)
+{
+if ((i%3 == 0) && (i%5 ==0))
+printf("FizzBuzz ");
+else if (i%3 == 0)
+printf("Fizz ");
+else if (i == 100)
+printf("Buzz\n");
+else if (i%5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i );
+}
+}
