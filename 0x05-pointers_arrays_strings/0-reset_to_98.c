ec40ffbfee10046ddc6d4345a83f2becbff65022
chedly99 <2276@holbertonschool.com>
2020-09-29 16:18:29 +0100

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
-* void reset_to_98- void reset to 98
+* void reset_to_98 - void reset to 98
 *@n: int
 *Return: 0
 */
 void reset_to_98(int *n)
 {
-  *n=98;
+*n = 98;
 }
