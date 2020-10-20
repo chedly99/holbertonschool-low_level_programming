83b59a3bcb1496666890084469152a4a13154759
chedly99 <2276@holbertonschool.com>
2020-10-20 22:21:03 +0100

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,6 @@
 void puts_half(char *str)
 {
 int i, j, n;
-
 for (i = 0; str[i]; i++)
 {}
 if (i % 2 == 0)
@@ -16,6 +15,14 @@ for (j = n; j < i; j++)
 {
 _putchar (str[j]);
 }
-_putchar ('\n');
 }
+else
+{
+n = (i + 1) / 2;
+for (j = n; j < i; j++)
+{
+_putchar (str[j]);
+}
+}
+_putchar ('\n');
 }
