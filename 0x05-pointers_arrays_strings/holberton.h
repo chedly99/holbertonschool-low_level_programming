3791d339468d5f79bca06c817421acb68459c182
chedly99 <2276@holbertonschool.com>
2020-10-20 20:48:15 +0100

    holberton

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,5 +7,8 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void print_array(int *a, int n);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
 
 #endif
