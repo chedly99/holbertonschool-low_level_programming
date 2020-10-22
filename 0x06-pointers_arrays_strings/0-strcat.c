1c32e806b74b258ac8cfdd179b0e51e354578ea4
chedly99 <2276@holbertonschool.com>
2020-10-22 17:18:00 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+**_strcat- concatenate two strings
+*@src: copied
+*@dest: destination
+*Return: string
+*/
+char *_strcat(char *dest, char *src)
+{
+char *s = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (s);
+}
