07cfb69190bf5818a7ace4c97e9d80c760f09914
chedly99 <2276@holbertonschool.com>
2020-10-22 17:48:26 +0100

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
 /**
-*_strncat - concatenates 2 string
-*@dest:char
-*@src:char
-*@n
-*Return:char
+* _strncat - concatenates two strings
+* @dest:char dest
+* @src: char src
+* @n:length int
+* Return:char
 */
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
-
 for (i = 0; dest[i] != '\0'; i++)
 {
 continue;
