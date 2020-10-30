1336245e539fe392bbbf0c35fab542f5baa5b58b
chedly99 <2276@holbertonschool.com>
2020-10-30 18:11:33 +0100

    rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - print recursion in reverse 
+* @s: arr
+* Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
