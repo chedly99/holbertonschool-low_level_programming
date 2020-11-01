2c7b8cc922f1316b2239956859b806f937422249
chedly99 <2276@holbertonschool.com>
2020-11-01 22:00:40 +0100

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints args
+* @argc: size of array argv
+* @argv: array
+* Return: int
+*/
+int main(int argc, char **argv)
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+int i, j;
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", (i *j));
+return (0);
+}
