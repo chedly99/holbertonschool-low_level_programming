3f29ff7c650969fc346e0a8565931f20a39fd173
chedly99 <2276@holbertonschool.com>
2020-11-01 22:04:38 +0100

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,13 @@
 */
 int main(int argc, char **argv)
 {
+int i;
+int j;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int i;
-int j;
 i = atoi(argv[1]);
 j = atoi(argv[2]);
 printf("%d\n", (i *j));
