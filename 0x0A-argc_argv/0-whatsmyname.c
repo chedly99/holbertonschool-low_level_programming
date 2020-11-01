dcb528629eb86256e270d85596716ae75dc61a5f
chedly99 <2276@holbertonschool.com>
2020-11-01 21:08:43 +0100

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - print program name
+  * @argc: size of array argv
+  * @argv: array
+  * Return: int
+  */
+
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+}
