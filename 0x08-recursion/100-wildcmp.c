886945f9e4827435d6bd3e2c27d7799b905ae135
chedly99 <2276@holbertonschool.com>
2020-11-02 01:23:32 +0100

    wildcmp

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*wildcmp - comparing 2 strings ?
+*@s1: string 1
+*@s2: string 2
+*Return: int
+*/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 && *s2)
+{
+}
+return (0);
+}
