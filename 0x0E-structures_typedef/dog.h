a4b1455d982527ba5d163ff432c1a1eb5f3030cd
chedly99 <2276@holbertonschool.com>
2020-11-06 11:51:26 +0100

    HeAdEr

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef DOG
-#define DOG
+#ifndef _Holberton_H_
+#define _Holberton_H_
+
 
 /**
 *struct dog - struct
@@ -14,5 +15,11 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
