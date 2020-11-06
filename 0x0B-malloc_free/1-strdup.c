1f0a1bde284dd35de4ea34d643d3084e564875c6
chedly99 <2276@holbertonschool.com>
2020-11-06 18:51:52 +0100

    fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,14 +1,32 @@
-#include "holberton.h"
 #include <stdlib.h>
 /**
-*_strdup - str dup
-*@str: 0str
+*length - the length of a string
+*@str: string
+*Return: length of str
+*/
+int length(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+i++;
+return (i);
+}
+/**
+*_strdup - string copies in it a string
+*@str: string
+*Return: a string
 */
 char *_strdup(char *str)
 {
-char *p = malloc(sizeof(*str));
-p = str;
-return (p);
+int i;
+char *s;
+if (str == NULL)
 return (NULL);
-
+s = malloc(sizeof(char) * length(str) + 1);
+if (s == NULL)
+return (NULL);
+for (i = 0; i <= length(str); i++)
+s[i] = str[i];
+s[length(str) + 1] = '\0';
+return (s);
 }
