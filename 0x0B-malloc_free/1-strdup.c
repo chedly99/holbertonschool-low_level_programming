d84f1dc3b32c30980bae09e4950d1da30729903c
chedly99 <2276@holbertonschool.com>
2020-11-06 18:40:30 +0100

    str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*_strdup - str dup
+*@str: 0str
+*/
+char *_strdup(char *str)
+{
+char *p = malloc(sizeof(char));
+p = str;
+return (p);
+return (NULL);
+
+}
