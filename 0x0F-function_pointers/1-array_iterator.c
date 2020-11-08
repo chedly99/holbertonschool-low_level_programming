08fbd26903a9c6cc01f4834e1d8d439c55fec6d6
chedly99 <2276@holbertonschool.com>
2020-11-08 23:17:57 +0100

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int i = 0;
 if (array == NULL || size == 0 || action == NULL)
 return;
-while (i < size)
+while (i > size)
 {
 i++
 }
