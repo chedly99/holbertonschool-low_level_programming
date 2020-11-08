66bd19f21dfe75e24d1af94b28fe739bb5865a6e
chedly99 <2276@holbertonschool.com>
2020-11-09 00:09:24 +0100

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,4 +11,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i = 0;
 if (array == NULL || size <= 0 || cmp == NULL)
 return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
 }
