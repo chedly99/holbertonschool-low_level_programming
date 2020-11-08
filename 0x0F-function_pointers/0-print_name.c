370304d76324c7ee5934e6a83a28a5b6b1cbbb75
chedly99 <2276@holbertonschool.com>
2020-11-08 21:55:15 +0100

    My Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_name - a program that adds positive numbers.
+*@name: the name to print.
+*@f: function to apply.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+
+ f(name);
+}
