f9bcee064b2427f690d4a2540dbeef4712dbde63
chedly99 <2276@holbertonschool.com>
2020-11-12 02:27:36 +0100

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,12 +12,13 @@ if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (len = 0; text_content[len]; len++)
+for (len = 0; text_content[len];)
+++len;
 }
 o = open(filename, O_WRONLY | O_APPEND);
 w = write(o, text_content, len);
 if (0 == -1 || w == -1)
-return(-1)
+return (-1);
 close(o);
 return (1);
 }
